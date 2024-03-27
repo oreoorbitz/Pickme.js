b7b2befb6205e6f19489b978188911932c2a9fe8
oreoorbitz <oreoorbitz@gmail.com>
2024-03-26 21:13:39 -0600

    Make more functional

--- modules/core/pickme.js
+++ modules/core/pickme.js
@@ -4,7 +4,7 @@ import frel from '../../modules/utils/frel.js'
 // logError :: String -> Maybe<null>
 const logError = message => {
   console.error(message)
-  return Maybe.of(null) // Encapsulating null to maintain chainability
+  return Maybe.of(null)
 }
 
 const querySelector = selector => Maybe.of(document.querySelector(selector))
@@ -12,135 +12,115 @@ const createElement = tagName => Maybe.of(document.createElement(tagName))
 
 class Pickme {
   constructor(namespace, tagName, attributes, innerText, eventHandlers) {
-    this.namespace = namespace // The namespace to identify elements.
-    this.tagName = tagName // The tag name for the root component.
-    this.attributes = attributes // Initial attributes for the root component.
-    this.innerText = innerText // Text content for the root component.
-    this.eventHandlers = eventHandlers // Event handlers for the root component.
-    this.components = {} // To store references to child components.
-    this.initRootComponent() // Initialize the root component.
+    this.namespace = namespace
+    this.tagName = tagName
+    this.attributes = attributes
+    this.innerText = innerText
+    this.eventHandlers = eventHandlers
+    this.components = {}
+    this.initRootComponent()
   }
+
 
   initRootComponent() {
     const rootSelector = `[data-pickme-component="${this.namespace}"]`
     const rootMaybe = querySelector(rootSelector).orElse(() => createElement(this.tagName))
-  
-    rootMaybe.map(maybeElement => {
-      console.log(maybeElement.val)
-      Object.entries(this.attributes).forEach(([key, value]) => {
-        maybeElement.val.setAttribute(key, value)
+
+    this.rootElement = rootMaybe
+      .map(element => {
+        Object.entries(this.attributes).forEach(([key, value]) => {
+          element.setAttribute(key, value)
+        })
+        element.textContent = this.innerText
+        Object.entries(this.eventHandlers).forEach(([event, handler]) => {
+          if (typeof handler === 'function') {
+            element.addEventListener(event, handler)
+          }
+        })
+        return element
       })
-      maybeElement.val.textContent = this.innerText
-      Object.entries(this.eventHandlers).forEach(([event, handler]) => {
-        if (typeof handler === 'function') {
-          maybeElement.val.addEventListener(event, handler)
-        }
+      .orElse(() => {
+        const appElement = querySelector('#app')
+        return appElement.map(app => {
+          app.appendChild(rootMaybe.val)
+          return rootMaybe.val
+        })
       })
-      return maybeElement
-    })
-  
-    rootMaybe.orElse(() => {
-      const appElement = querySelector('#app').val
-      appElement.appendChild(rootMaybe.val)
-      return rootMaybe
-    })
-  
-    this.rootElement = rootMaybe.val
   }
 
-  // Registers a component as a child of another component.
   component(parentComponentName, componentName, tagName, attributes, innerText) {
     const parentSelector = `[data-pickme-component="${parentComponentName}"]`
     const parentMaybe = querySelector(parentSelector)
-  
+
     const childSelector = `[data-pickme-component="${componentName}"]`
     const childMaybe = querySelector(childSelector)
-  
-    childMaybe.map(element => {
-      console.log(element)
-      const attributesMatch = Object.entries(attributes).every(([key, value]) => element.val.getAttribute(key) === value)
-      const innerTextMatch = element.val.textContent === innerText
-  
-      if (!attributesMatch || !innerTextMatch) {
-        element.val.remove()
-        logError(`Component "${componentName}" exists but doesn't match the specified attributes and inner text.`)
-      }
-    })
-  
+
+    const attributesMatch = childMaybe.map(element =>
+      Object.entries(attributes).every(([key, value]) => element.getAttribute(key) === value)
+    )
+    const innerTextMatch = childMaybe.map(element => element.textContent === innerText)
+
+    childMaybe
+      .map(element => attributesMatch.val && innerTextMatch.val ? element : element.remove())
+      .orElse(() => logError(`Component "${componentName}" exists but doesn't match the specified attributes and inner text.`))
+
     if (childMaybe.isNothing()) {
       const childElement = createElement(tagName).map(element => {
         Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value))
         element.textContent = innerText
         return element
       })
-  
-      liftAppendChild(childElement)(parentMaybe)
-      this.components[componentName] = childElement
+
+      parentMaybe.map(parent => {
+        liftAppendChild(childElement)(Maybe.of(parent))
+        this.components[componentName] = childElement
+      })
     }
   }
 
   registerEvent(eventName, handler) {
-    Object.entries(this.components).forEach(([componentName, maybeElement]) => {
-      maybeElement.val.addEventListener(eventName, handler)
+    Object.entries(this.components).forEach(([_, maybeElement]) => {
+      maybeElement.map(element => element.addEventListener(eventName, handler))
     })
   }
 
-  // Modifies an existing component in the DOM.
   modify(componentName, tagName, attributes, innerText) {
     const selector = `[data-pickme-component="${componentName}"]`
     const componentMaybe = querySelector(selector)
 
-    componentMaybe.map(element => {
-      const attributesMatch = Object.entries(attributes).every(([key, value]) => element.getAttribute(key) === value)
-      const innerTextMatch = element.textContent === innerText
+    const attributesMatch = componentMaybe.map(element =>
+      Object.entries(attributes).every(([key, value]) => element.getAttribute(key) === value)
+    )
+    const innerTextMatch = componentMaybe.map(element => element.textContent === innerText)
 
-      if (!attributesMatch || !innerTextMatch) {
-        element.remove()
-        logError(`Component "${componentName}" exists but doesn't match the specified attributes and inner text.`)
-      }
+    componentMaybe
+      .map(element => attributesMatch.val && innerTextMatch.val ? element : element.remove())
+      .orElse(() => logError(`Component "${componentName}" exists but doesn't match the specified attributes and inner text.`))
+      .orElse(() => logError(`Component "${componentName}" doesn't exist in the DOM.`))
 
-      return element
-    })
-
-    if (componentMaybe.isNothing()) {
-      logError(`Component "${componentName}" doesn't exist in the DOM.`)
-    }
-
-    return this // For method chaining.
+    return this
   }
 
-  // Adds a class to a component.
   addClass(componentName, className) {
     const selector = `[data-pickme-component="${componentName}"]`
     const componentMaybe = querySelector(selector)
 
-    componentMaybe.map(element => {
-      element.classList.add(className)
-      return element
-    })
+    componentMaybe
+      .map(element => element.classList.add(className))
+      .orElse(() => logError(`Component "${componentName}" doesn't exist in the DOM.`))
 
-    if (componentMaybe.isNothing()) {
-      logError(`Component "${componentName}" doesn't exist in the DOM.`)
-    }
-
-    return this // For method chaining.
+    return this
   }
 
-  // Sets the innerText for a component.
   innerText(componentName, text) {
     const selector = `[data-pickme-component="${componentName}"]`
     const componentMaybe = querySelector(selector)
 
-    componentMaybe.map(element => {
-      element.textContent = text
-      return element
-    })
+    componentMaybe
+      .map(element => element.textContent = text)
+      .orElse(() => logError(`Component "${componentName}" doesn't exist in the DOM.`))
 
-    if (componentMaybe.isNothing()) {
-      logError(`Component "${componentName}" doesn't exist in the DOM.`)
-    }
-
-    return this // For method chaining.
+    return this
   }
 }
 
